970d16521c56bbf2aa0747d31f830a831684a56c
PRATHAM <6230809462game@gmail.com>
2024-11-23 13:14:20 +0530

    h

--- class3.py
+++ class3.py
@@ -7,14 +7,14 @@
 # 3. logical not operator
 # print(10>5 and 13>8 and 20>10 and 76>8)
 # print (not 12>100)
-age =20
+# age =20
 
 # if age>18:
 # print("you can vote")
 # if age>8 and age <8:
 #     print("you are child")
-a=56
-if a<=56.0 and 41>36:
-    print("hello")
-  else:
-  print("world")
+# a=56
+# if a<=56.0 and 41>36:
+#     print("hello")
+#   else:
+#   print("world")
