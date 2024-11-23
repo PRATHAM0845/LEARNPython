934a9760beff736c6d4767fb33230a45e62ecd54
PRATHAM <6230809462game@gmail.com>
2024-11-23 13:02:53 +0530

    hi

---
+++ class3.py
@@ -0,0 +1,20 @@
+ #data types
+# a=10.5
+
+# print(13.999<=13.199)
+#1. logical and opeator
+# 2. logical or operator 
+# 3. logical not operator
+# print(10>5 and 13>8 and 20>10 and 76>8)
+# print (not 12>100)
+age =20
+
+# if age>18:
+# print("you can vote")
+# if age>8 and age <8:
+#     print("you are child")
+a=56
+if a<=56.0 and 41>36:
+    print("hello")
+  else:
+  print("world")
